e2e0fc9e0904e08dbfc970e8edd81b940affc504
Arjen de Ruijter <ruijter@arjen.de>
2022-10-05 15:27:31 +0200

    OpenSSL support functions

---
+++ src/DWL.OpenSSL.pas
@@ -0,0 +1,287 @@
+unit DWL.OpenSSL;
+
+interface
+
+uses
+  System.SysUtils, DWL.OpenSSL.Api;
+
+type
+  IdwlOpenSSLKey = interface
+    function key: pEVP_PKEY;
+    function Modulus_n: TBytes;
+    function PublicExponent_e: TBytes;
+  end;
+
+  TdwlOpenSSL = record
+  public
+    class function New_PrivateKey: IdwlOpenSSLKey; static;
+    class function New_PrivateKey_FromPEMFile(const FileName: string): IdwlOpenSSLKey; static;
+    class function New_PrivateKey_FromPEMStr(const PEM: string): IdwlOpenSSLKey; static;
+    class function New_PublicKey_FromModulusExponent(const Modulus, Exponent: TBytes): IdwlOpenSSLKey; static;
+    class procedure PrivateKey_SaveToPEMFile(PrivateKey: IdwlOpenSSLKey; const FileName: string); static;
+    class function Calculate_SHA256(const Data: ansistring): TBytes; overload; static;
+    class function Calculate_SHA256(const Data: PByte; DataSize: NativeUInt): TBytes; overload; static;
+    class function Calculate_SHA512(const Data: ansistring): TBytes; static;
+    class function CalculateSignature(const Data: ansistring; PrivateKey: IdwlOpenSSLKey): TBytes; static;
+    class function VerifySignature(const Data: ansistring; const Signature: TBytes; PublicKey: IdwlOpenSSLKey): boolean; static;
+    /// <summary>
+    ///   Generates <Amount> random bytes using a cryptographically secure pseudo random generator (CSPRNG)
+    /// </summary>
+    class function RandomBytes(Amount: integer): TBytes; static;
+    // derives a bas64url encoded key from a base64url hash salt and a plain password using PBKDF2 and HMAC-SHA256
+    /// <summary>
+    ///   Derives a Key (base64url encoded) from a salt(bat64url encoded) and a plain password
+    /// </summary>
+    class function DeriveKeyFromPassword(const Salt_b64utf, Password_plain: string): string; static;
+    class function ASN1_StringToDateTime(x: pASN1_STRING): TDateTime; static;
+  end;
+
+implementation
+
+uses
+  Winapi.Windows, System.DateUtils, System.NetEncoding;
+
+type
+  TdwlOpenSSLKey = class(TInterfacedObject, IdwlOpenSSLKey)
+    FKey: pEVP_PKEY;
+  private
+    function key: pEVP_PKEY;
+    function Modulus_n: TBytes;
+    function PublicExponent_e: TBytes;
+  public
+    constructor Create(AKey: pEVP_PKEY);
+    destructor Destroy; override;
+  end;
+
+{ TdwlOpenSSL }
+
+class function TdwlOpenSSL.ASN1_StringToDateTime(x: pASN1_STRING): TDateTime;
+  function Char2Int(d, u: AnsiChar): integer;
+  begin
+    result := (Ord(d) - Ord('0'))*10 + Ord(u) - Ord('0');
+  end;
+begin
+  var tz := 0;
+  var s := 0;
+  var Y := 0;
+  var M := 0;
+  var D := 0;
+  var h := 0;
+  var n := 0;
+  var StrLen := ASN1_STRING_length(x);
+  var AsnStr := ASN1_STRING_get0_data(x); 
+  if StrLen = 13 then
+    begin
+      Y := Char2Int(AsnStr[0], AsnStr[1]);
+      if Y < 50 then
+        Y := Y + 100;
+      Y := Y + 1900;
+      M := Char2Int(AsnStr[2], AsnStr[3]);
+      D := Char2Int(AsnStr[4], AsnStr[5]);
+      h := Char2Int(AsnStr[6], AsnStr[7]);
+      n := Char2Int(AsnStr[8], AsnStr[9]);
+      if (AsnStr[10] >= '0') and (AsnStr[10] <= '9')
+          and (AsnStr[11] >= '0') and (AsnStr[11] <= '9') then
+        s := Char2Int(AsnStr[10], AsnStr[11]);
+      if AsnStr[12] = 'Z' then
+        tz := 1;
+    end
+  else if StrLen = 15 then
+    begin
+      Y := Char2Int(AsnStr[0], AsnStr[1])*100 + Char2Int(AsnStr[2], AsnStr[3]);;
+      M := Char2Int(AsnStr[4], AsnStr[5]);
+      D := Char2Int(AsnStr[6], AsnStr[7]);
+      h := Char2Int(AsnStr[8], AsnStr[9]);
+      n := Char2Int(AsnStr[10], AsnStr[11]);
+      if (AsnStr[12] >= '0') and (AsnStr[12] <= '9')
+          and (AsnStr[13] >= '0') and (AsnStr[13] <= '9') then
+        s := Char2Int(AsnStr[12], AsnStr[13]);
+      if AsnStr[14] = 'Z' then
+        tz := 1;
+    end;
+  if tz > 0 then
+    Result := IncHour(EncodeDateTime(Y, M, D, h, n, s, 0), tz)
+  else
+    Result := EncodeDateTime(Y, M, D, h , n, s, 0);
+end;
+
+class function TdwlOpenSSL.CalculateSignature(const Data: ansistring; PrivateKey: IdwlOpenSSLKey): TBytes;
+begin
+  var mdctx := EVP_MD_CTX_new;
+  try
+    EVP_DigestSignInit(mdctx, nil, EVP_sha256, nil, PrivateKey.key);
+    EVP_DigestSignUpdate(mdctx, PAnsiChar(Data), Length(Data));
+    var ResultSize: size_t := 512;
+    SetLength(Result, ResultSize);
+    if EVP_DigestSignFinal(mdctx, PAnsiChar(@Result[0]), ResultSize)<>1 then
+      raise Exception.Create('Error Message');
+    SetLength(Result, ResultSize);
+    EVP_DigestSignFinal(mdctx, PAnsiChar(@Result[0]), ResultSize);
+  finally
+    EVP_MD_CTX_free(mdctx);
+  end;
+end;
+
+class function TdwlOpenSSL.Calculate_SHA256(const Data: ansistring): TBytes;
+begin
+  SetLength(Result, SHA256_DIGEST_LENGTH);
+  SHA256(PAnsiChar(Data), Length(Data), @Result[0]);
+end;
+
+class function TdwlOpenSSL.Calculate_SHA256(const Data: PByte; DataSize: NativeUInt): TBytes;
+begin
+  SetLength(Result, SHA256_DIGEST_LENGTH);
+  SHA256(PAnsiChar(Data), DataSize,  @Result[0]);
+end;
+
+class function TdwlOpenSSL.Calculate_SHA512(const Data: ansistring): TBytes;
+begin
+  SetLength(Result, SHA512_DIGEST_LENGTH);
+  SHA512(PAnsiChar(Data), Length(Data), @Result[0]);
+end;
+
+class function TdwlOpenSSL.DeriveKeyFromPassword(const Salt_b64utf, Password_plain: string): string;
+const
+  Iterations = 131072; // slow enough.. (f.e. LastPass uses 5000)
+  OctetCount = 64; // big enought (f.e. Lastpas uses 32)
+  // 2021-12-11 AdR calculation time on my development machine: 100ms
+  // If needed these constants can become optional parameters if needed in the future
+begin
+  // hashing using PBKDF2 and HMAC-SHA256 (both FIPS validated implementations)
+  var SaltBytes: TBytes := TNetencoding.Base64URL.DecodeStringToBytes(Salt_b64utf);
+  var Password_utf8 := UTF8String(Password_plain);
+  var Bytes: TBytes;
+  SetLength(Bytes, OctetCount);
+  if PKCS5_PBKDF2_HMAC(PAnsichar(PUTF8String(Password_utf8)), Length(Password_utf8), @SaltBytes[0], Length(SaltBytes),
+    Iterations, EVP_sha256, OctetCount, @Bytes[0])<>1 then
+    raise Exception.Create('Error executing key derivation function');
+  Result := TNetEncoding.Base64URL.EncodeBytesToString(Bytes);
+end;
+
+class function TdwlOpenSSL.New_PrivateKey_FromPEMFile(const FileName: string): IdwlOpenSSLKey;
+begin
+  var BIO := BIO_new_file(PAnsiChar(ansistring(FileName)), PAnsiChar('r+'));
+  try
+    var key := PEM_read_bio_PrivateKey(BIO, nil, nil, nil);
+    if key=nil then
+      Result := nil
+    else
+      Result := TdwlOpenSSLKey.Create(key);
+  finally
+    BIO_free(BIO);
+  end;
+end;
+
+class function TdwlOpenSSL.New_PrivateKey_FromPEMStr(const PEM: string): IdwlOpenSSLKey;
+begin
+  var BIO := BIO_new(BIO_s_mem);
+  try
+    var AnsiPEM := ansistring(PEM);
+    BIO_write(BIO, PAnsiChar(AnsiPEM), Length(AnsiPEM));
+    var key := PEM_read_bio_PrivateKey(BIO, nil, nil, nil);
+    if key=nil then
+      Result := nil
+    else
+      Result := TdwlOpenSSLKey.Create(key);
+  finally
+    BIO_Free(BIO);
+  end;
+end;
+
+class function TdwlOpenSSL.New_PrivateKey: IdwlOpenSSLKey;
+begin
+  Result := TdwlOpenSSLKey.Create(EVP_PKEY_Q_keygen(nil, nil, PAnsiChar('RSA'), 4096));
+end;
+
+class function TdwlOpenSSL.New_PublicKey_FromModulusExponent(const Modulus, Exponent: TBytes): IdwlOpenSSLKey;
+begin
+  var key := EVP_PKEY_new;
+  var BN_n := BN_bin2bn(@Modulus[0], Length(Modulus), nil);
+  var BN_e := BN_bin2bn(@Exponent[0], Length(Exponent), nil);
+  try
+    EVP_PKEY_set_bn_param(key, 'n', BN_n);
+    EVP_PKEY_set_bn_param(key, 'e', BN_e);
+  finally
+    BN_free(BN_n);
+    BN_free(BN_e);
+  end;
+  Result := TdwlOpenSSLKey.Create(key);
+end;
+
+class function TdwlOpenSSL.RandomBytes(Amount: integer): TBytes;
+begin
+  SetLength(Result, Amount);
+  if RAND_bytes(@Result[0], Amount)<>1 then
+    raise Exception.Create('Error generating random bytes');
+end;
+
+class procedure TdwlOpenSSL.PrivateKey_SaveToPEMFile(PrivateKey: IdwlOpenSSLKey; const FileName: string);
+begin
+  var bp := BIO_new_file(PAnsiChar(ansistring(FileName)), PAnsiChar('w+'));
+  try
+    PEM_write_bio_PrivateKey(bp, PrivateKey.key, nil, nil, 0, nil, nil);
+  finally
+    BIO_free(bp);
+  end;
+end;
+
+class function TdwlOpenSSL.VerifySignature(const Data: ansistring; const Signature: TBytes; PublicKey: IdwlOpenSSLKey): boolean;
+begin
+  var mdctx := EVP_MD_CTX_new;
+  try
+    EVP_DigestVerifyInit(mdctx, nil, EVP_sha256, nil, PublicKey.key);
+    EVP_DigestVerifyUpdate(mdctx, PAnsiChar(Data), Length(Data));
+    Result := EVP_DigestVerifyFinal(mdctx, PAnsiChar(@Signature[0]), Length(Signature))=1;
+  finally
+    EVP_MD_CTX_free(mdctx);
+  end;
+end;
+
+{ TopensslKey }
+
+constructor TdwlOpenSSLKey.Create(AKey: pEVP_PKEY);
+begin
+  inherited Create;
+  FKey := AKey;
+end;
+
+destructor TdwlOpenSSLKey.Destroy;
+begin
+  EVP_PKEY_free(FKey);
+  inherited Destroy;
+end;
+
+function TdwlOpenSSLKey.PublicExponent_e: TBytes;
+begin
+  var bn := BN_new;
+  try
+    if EVP_PKEY_get_bn_param(key, 'e', bn)<>1 then
+      raise Exception.Create('Error retrieving e');
+    var BN_Size := BN_num_bytes(bn);
+    SetLength(Result, BN_Size);
+    BN_bn2bin(BN, @Result[0]);
+  finally
+    BN_free(bn);
+  end;
+end;
+
+function TdwlOpenSSLKey.key: pEVP_PKEY;
+begin
+  Result := FKey;
+end;
+
+function TdwlOpenSSLKey.Modulus_n: TBytes;
+begin
+  var bn := BN_new;
+  try
+    if EVP_PKEY_get_bn_param(key, 'n', bn)<>1 then
+      raise Exception.Create('Error retrieving n');
+    var BN_Size := BN_num_bytes(bn);
+    SetLength(Result, BN_Size);
+    BN_bn2bin(BN, @Result[0]);
+  finally
+    BN_free(bn);
+  end;
+end;
+
+end.
