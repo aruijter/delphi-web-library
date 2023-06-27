012f72ea555f2a972cbc6bd3e5466aef440b9dcb
Arjen de Ruijter <ruijter@arjen.de>
2023-06-27 22:35:20 +0200

    DWL.StrUtils: added Char modifications

--- src/DWL.StrUtils.pas
+++ src/DWL.StrUtils.pas
@@ -2,12 +2,19 @@ unit DWL.StrUtils;
 
 interface
 
+uses
+  System.SysUtils;
+
 type
-  TdwlSanatizeOption = (soRemoveInvalidFileNameChars, soRemoveInvalidPathChars, soRemoveLineBreaks);
+  TdwlSanatizeOption = (soRemoveInvalidFileNameChars, soRemoveInvalidPathChars, soRemoveLineBreaks, soOnlyAZ);
   TdwlSanatizeOptions = set of TdwlSanatizeOption;
 
   TdwlStrUtils = record
-    class function RemoveChars(const Value: string; CharsToRemove: array of char): string; static;
+    class function CharInArray(const C: Char; const InArray: TCharArray): boolean; static;
+    class function RemoveChars(const Value: string; CharsToRemove: TCharArray): string; overload; static;
+    class function RemoveChars(const Value: string; CharsToRemove: TSysCharSet): string; overload; static;
+    class function FilterChars(const Value: string; CharsToAllow: TCharArray): string; overload; static;
+    class function FilterChars(const Value: string; CharsToAllow: TSysCharSet): string; overload; static;
     class function Sanatize(const Value: string; Options: TdwlSanatizeOptions): string; static;
     class function TrimLeft(const Value, TrimStr: string; IgnoreCase: boolean=false): string; static;
   end;
@@ -15,18 +22,58 @@ type
 implementation
 
 uses
-  System.SysUtils, System.IOUtils;
+  System.IOUtils;
 
 { TdwlStrUtils }
 
-class function TdwlStrUtils.RemoveChars(const Value: string; CharsToRemove: array of char): string;
-  function CharInArray(const C: Char; const InArray: array of Char): Boolean;
+class function TdwlStrUtils.CharInArray(const C: Char; const InArray: TCharArray): boolean;
+begin
+  for var AChar in InArray do
+    if AChar = C then
+      Exit(True);
+  Result := False;
+end;
+
+class function TdwlStrUtils.FilterChars(const Value: string; CharsToAllow: TCharArray): string;
+begin
+  Result := Value;
+  var i := 1;
+  while i<=length(Result) do
   begin
-    for var AChar in InArray do
-      if AChar = C then
-        Exit(True);
-    Result := False;
+    if not CharInArray(Result[i], CharsToAllow) then
+      Result := Result.SubString(0, i-1)+Result.Substring(i)
+    else
+      inc(i);
   end;
+end;
+
+class function TdwlStrUtils.FilterChars(const Value: string; CharsToAllow: TSysCharSet): string;
+begin
+  Result := Value;
+  var i := 1;
+  while i<=length(Result) do
+  begin
+    if not CharInSet(Result[i], CharsToAllow) then
+      Result := Result.SubString(0, i-1)+Result.Substring(i)
+    else
+      inc(i);
+  end;
+end;
+
+class function TdwlStrUtils.RemoveChars(const Value: string; CharsToRemove: TSysCharSet): string;
+begin
+  Result := Value;
+  var i := 1;
+  while i<=length(Result) do
+  begin
+    if CharInSet(Result[i], CharsToRemove) then
+      Result := Result.SubString(0, i-1)+Result.Substring(i)
+    else
+      inc(i);
+  end;
+end;
+
+class function TdwlStrUtils.RemoveChars(const Value: string; CharsToRemove: TCharArray): string;
 begin
   Result := Value;
   var i := 1;
@@ -42,6 +89,8 @@ end;
 class function TdwlStrUtils.Sanatize(const Value: string; Options: TdwlSanatizeOptions): string;
 begin
   Result := Value;
+  if (soOnlyAZ in Options) then
+    Result := FilterChars(Result, ['A'..'Z','a'..'z']);
   if (soRemoveInvalidFileNameChars in Options) then
     Result := RemoveChars(Result, TPath.GetInvalidFileNameChars);
   if (soRemoveInvalidPathChars in Options) then
