1f428ae0b45848faeb6d7c230d0dc3f82aa4e84c
Arjen de Ruijter <ruijter@arjen.de>
2023-11-26 18:13:03 +0100

    fixed TdwlTValuehelper.equals (for strings and some other corner cases)

--- src/DWL.Rtti.Utils.pas
+++ src/DWL.Rtti.Utils.pas
@@ -20,16 +20,23 @@ uses
 function TdwlTValueHelper.Equals(CompareValue: TValue): boolean;
 begin
   // We take for granted than TTypeInfo instances are unqiue, so we simply can compare pointers
-  Result := Self.TypeInfo=CompareValue.TypeInfo;
+  Result := TypeInfo=CompareValue.TypeInfo;
   if not Result then
     Exit;
-  // of course have the same size
-  var Size := Self.DataSize;
-  Result := Size=CompareValue.DataSize;
+  // of course have the same datasize
+  var DtSize := DataSize;
+  Result := DtSize=CompareValue.DataSize;
   if not Result then
     Exit;
-  // and having the same raw data
-  Result := CompareMem(Self.GetReferenceToRawData, CompareValue.GetReferenceToRawData, Size);
+  case TypeInfo.Kind of
+    tkInteger, tkEnumeration, tkChar, tkWChar, tkFloat, tkInt64,tkRecord, tkMRecord, tkArray, tkVariant:
+        Result := (DtSize>0) and CompareMem(GetReferenceToRawData, CompareValue.GetReferenceToRawData, DtSize);
+    tkString: Result := PShortString(GetReferenceToRawData)^=PShortString(CompareValue.GetReferenceToRawData)^;
+    tkWString: Result := PWideString(GetReferenceToRawData)^=PWideString(CompareValue.GetReferenceToRawData)^;
+    tkLString: Result := PRawByteString(GetReferenceToRawData)^=PRawByteString(CompareValue.GetReferenceToRawData)^;
+    tkUString: Result := PUnicodeString(GetReferenceToRawData)^=PUnicodeString(CompareValue.GetReferenceToRawData)^;
+    tkSet, tkClass, tkClassRef, tkMethod, tkInterface, tkPointer, tkDynArray: Result := false;
+  end;
 end;
 
 end.
